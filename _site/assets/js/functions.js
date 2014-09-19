b03d066a7ef7b9cc2d7b7c844a07c008cd90432e
cclaro <carlosclaro@Carloss-MacBook-Air.local>
2014-09-19 22:38:59 +1000

    Video 7 in progress
     - added scroll function JS

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,4 +1,17 @@
-$( document ).ready(function() {
-	//
-
+$(function() {
+	smoothScrool(1000);
 });
+
+	function smoothScrool (duration) {
+		$('a[href^="#"]').on('click', function(event) {
+
+			var target = $( $(this).attr('href') );
+
+			if( target.length ) {
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, duration);
+			}
+		});
+	}
